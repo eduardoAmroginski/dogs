6594deba5b1d61ac299935f995393788651c1ee3
Eduardo Amroginski <eduardo-amroginski@hotmail.com>
2021-08-14 02:07:42 -0300

    wip: Fazendo refactor do componente Input, adicionando useForm, api.js e fazendo fetch para pegar usuários

--- src/Components/Forms/Input/Input.jsx
+++ src/Components/Forms/Input/Input.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import styles from './Input.module.css';
+import React from "react";
+import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input type={type} id={name} name={name} className={styles.input} />
-      <p className={styles.error}>Erro</p>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className={styles.input}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
--- src/Components/Login/LoginForm/LoginForm.jsx
+++ src/Components/Login/LoginForm/LoginForm.jsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../../Forms/Button/Button';
-import Input from '../../Forms/Input/Input';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { TOKEN_POST, USER_GET } from "../../../api";
+
+import useForm from "../../../Hooks/useForm";
+import Button from "../../Forms/Button/Button";
+import Input from "../../Forms/Input/Input";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const username = useForm();
+  const password = useForm();
 
-  function handleSubmit (event) {
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    const data = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    };
-
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', data)
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(json => {
-        console.log(json);
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
       });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
-      <form action='' onSubmit={handleSubmit}>
-        <Input label='Usuário' type='text' name='username' />
-        <Input label='Senha' type='password' name='password' />
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
-      <Link to='/login/criar'>Cadastro</Link>
+      <Link to="/login/criar">Cadastro</Link>
     </section>
-
   );
 };
 
