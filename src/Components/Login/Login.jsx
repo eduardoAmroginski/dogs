101dd23fab91be681465c7ce3d96f73add486401
Eduardo Amroginski <eduardo-amroginski@hotmail.com>
2021-08-14 16:25:38 -0300

    wip: Estilizando Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -8,19 +8,23 @@ import LoginPasswordReset from "./LoginPasswordReset/LoginPasswordReset";
 
 import { UserContext } from "../../Contexts/UserContext";
 
+import styles from "./Login.module.css";
+
 const Login = () => {
   const { login } = useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
