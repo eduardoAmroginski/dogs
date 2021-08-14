237e0b3266173ca29ef74029e60429ea68ad710f
Eduardo Amroginski <eduardo-amroginski@hotmail.com>
2021-08-14 02:36:11 -0300

    Adicionando UserContext

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
-import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./Contexts/UserContext";
 
-function App () {
+function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login/*' element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
-import { ReactComponent as Dogs } from '../../Assets/dogs.svg';
+import { ReactComponent as Dogs } from "../../Assets/dogs.svg";
+
+import { UserContext } from "../../Contexts/UserContext";
 
 const Header = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to='/' aria-label='Dogs - Home'>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link className={styles.login} to='/login'>
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
