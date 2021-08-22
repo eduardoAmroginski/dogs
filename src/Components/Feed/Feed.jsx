126d5d55c0c5d9511f094414bb083593937fd7bd
Eduardo Amroginski <eduardo-amroginski@hotmail.com>
2021-08-22 20:19:19 -0300

    wip: Adicionando propTypes ao Feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import FeedModal from "./FeedModal/FeedModal";
 
+import FeedModal from "./FeedModal/FeedModal";
 import FeedPhotos from "./FeedPhotos/FeedPhotos";
+
+import PropTypes from "prop-types";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = useState(null);
@@ -48,6 +50,17 @@ const Feed = ({ user }) => {
       ))}
     </div>
   );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
 };
 
 export default Feed;
